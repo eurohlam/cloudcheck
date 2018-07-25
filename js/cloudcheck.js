b516fb9ead288205f364ce15ad73375630ef0cee
eurohlam <eurohlam@gmail.com>
2018-07-25 16:14:49 +1200

    updated response handling in javascript

--- js/cloudcheck.js
+++ js/cloudcheck.js
@@ -9,6 +9,7 @@
         // get values from FORM
         var name = $("input#name").val();
         var surname = $("input#surname").val();
+		var middlename = $("input#middlename").val();
         var dateofbirth = $("input#dateofbirth").val();
         var city = $("input#city").val();
         var suburb = $("input#suburb").val();
@@ -23,6 +24,9 @@
         var data = { 'details' : {}, 'reference' : '1', 'consent': 'Yes', 'capturereference': 'a09b1dc5-ea4f-4591-9e44-1fca76dfd000' };
 
         data.details.name = { 'given' : name, 'family': surname };
+		if ( middlename ) {
+			data.details.name.middle = middlename;
+		}
         data.details.dateofbirth = dateofbirth;
         data.details.address = { 'city' : city,
                                  'suburb' : suburb,
@@ -56,15 +60,23 @@
             cache: false,
             success: function(data) {
                 console.log("Cloudcheck response: " + JSON.stringify(data));
-                // Enable button & show success message
-                $("#btnSubmit").attr("disabled", false);
-                $('#success').html("<div class='alert alert-success'>");
-                $('#success > .alert-success').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
-                    .append("</button>");
-                $('#success > .alert-success')
-                    .append("<strong>Verification response: </strong>" + JSON.stringify(data));
-                $('#success > .alert-success')
-                    .append('</div>');
+				var ref = data.verification.verificationReference;
+				var error = data.verification.errorDetail;
+				console.log("Ref: " + ref);
+				console.log("Error: " + error);	
+				if ( ref ) {
+					getPdf(ref);
+				} else if ( error ) {
+	                // Enable button & show success message
+	                $("#btnSubmit").attr("disabled", false);
+	                $('#success').html("<div class='alert alert-danger'>");
+	                $('#success > .alert-danger').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
+	                    .append("</button>");
+	                $('#success > .alert-danger')
+	                    .append("<strong>Verification error: </strong>" + error + " <p>" +JSON.stringify(data) + "</p>");
+	                $('#success > .alert-danger')
+	                    .append('</div>');
+				}
 
                 //clear all fields
                 $('#cloudcheckForm').trigger("reset");
@@ -84,7 +96,6 @@
 
 
     $("#cloudcheckPdfForm").submit(function(event) {
-
         // Prevent spam click and default submit behaviour
         $("#btnGetPdf").attr("disabled", true);
         event.preventDefault();
@@ -92,6 +103,14 @@
         // get values from FORM
         var reference = $("input#verificationReference").val();
         console.log("Cloudcheck request: " + reference);
+		getPdf(reference);
+        // Enable button & show success message
+        $("#btnGetPdf").attr("disabled", false);
+        //clear all fields
+        $('#cloudcheckPdfForm').trigger("reset");
+    }); //cloudcheckPdfForm submit
+
+	function getPdf(reference) {
 
         $.ajax({
             url: "/wp-admin/admin-ajax.php", //"wp-content/plugins/cloudcheck/cloudcheck.php",
@@ -105,8 +124,6 @@
             cache: false,
             success: function(data) {
                 console.log("Cloudcheck response: " + JSON.stringify(data));
-                // Enable button & show success message
-                $("#btnGetPdf").attr("disabled", false);
                 $('#success').html("<div class='alert alert-success'>");
                 $('#success > .alert-success').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
                     .append("</button>");
@@ -115,8 +132,6 @@
                 $('#success > .alert-success')
                     .append('</div>');
 
-                //clear all fields
-                $('#cloudcheckPdfForm').trigger("reset");
                 //open pdf in new tab
                 window.open(data.pdf, '_blank');
             },
@@ -125,12 +140,12 @@
                 $('#success').html("<div class='alert alert-danger'>");
                 $('#success > .alert-danger').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
                     .append("</button>");
-                $('#success > .alert-danger').append("<strong>Sorry " + name + ", it seems that cloudcheck service is not responding. Please try again later!");
+                $('#success > .alert-danger').append("<strong>It seems that Cloudcheck service is not responding. Verification is done, but we are not able to get resulted PDF. Please try again later!");
                 $('#success > .alert-danger').append('</div>');
                 //clear all fields
                 $('#cloudcheckPdfForm').trigger("reset");
             },
         });
-    }); //cloudcheckPdfForm submit
+	} //getPdf 
 
 }) ( jQuery );
