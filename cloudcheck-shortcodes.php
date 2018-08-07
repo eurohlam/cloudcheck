1ac08d3834d2395bcdc6785e407ecdc8b0666996
eurohlam <eurohlam@gmail.com>
2018-08-07 18:40:19 +1200

    fixed shortcodes

--- cloudcheck-shortcodes.php
+++ cloudcheck-shortcodes.php
@@ -4,10 +4,19 @@ function cloudcheck_shortcodes_init()
 
     function cloudcheck_main_form_shortcode($atts = [], $content = null)
     {
-        $form = '<head><link rel="stylesheet" href="css/bootstrap.css"></head><section id="cloudcheck_section"><div class="container"><div class="row"><div class="col-lg-8 col-lg-offset-2"><form id="cloudcheckForm">';
-        $form = $form . $content;
-		$form = $from . '<div id="success"></div><div class="row"><div class="form-group col-xs-12"><button id="btnSubmit" class="btn btn-success btn-lg" type="submit">Send</button></div></div></form></div></div></section>';
-		$form = $form . '<script type="text/javascript" src="/wp-content/plugins/cloudcheck/js/cloudcheck.js"></script>'; 
+        $form = '<head><link rel="stylesheet" href="/wp-content/plugins/cloudcheck/css/bootstrap.css"></head>
+            <section id="cloudcheck_section">
+                <div class="container"><div class="row"><div class="col-lg-8 col-lg-offset-2">
+                <form id="cloudcheckForm">'
+                    . do_shortcode($content)
+		            . '<div id="success"></div><div class="row">
+                    <div class="form-group col-xs-12">
+                        <button id="btnSubmit" class="btn btn-success btn-lg" type="submit">Send</button>
+                    </div></div>
+                </form>
+                </div></div>
+            </section>
+		    <script type="text/javascript" src="/wp-content/plugins/cloudcheck/js/cloudcheck.js"></script>';
         return $form;
     }
     add_shortcode('cloudcheck_main_form', 'cloudcheck_main_form_shortcode');
@@ -15,11 +24,11 @@ function cloudcheck_shortcodes_init()
 
 	/** Shortcode that shows basic fields always required for verification:
 	 *  first name, middle name, surname, address
-	 */ 	
+	 */
     function cloudcheck_basic_info_shortcode($atts = [], $content = null)
     {
         // do something to $content
- 
+
         // always return
         return $content;
     }
@@ -28,11 +37,19 @@ function cloudcheck_shortcodes_init()
 
 	/** Shortcode that shows fields for verification by NZ passport:
 	 *  passport number, passport expiry
-	 */ 	
+	 */
     function cloudcheck_nz_passport_shortcode($atts = [], $content = null)
     {
-        $passport = '<div class="row control-group"><div class="form-group col-xs-12 floating-label-form-group controls"><label>Passport number</label><input id="passportnumber" class="form-control" type="text" placeholder="Passport number" /></div><div class="form-group col-xs-12 floating-label-form-group controls"><label>Passport Expiry</label><input id="passportexpiry" class="form-control" type="date" placeholder="Passport expiry" /></div></div>';
- 
+        $passport = '<div class="row control-group">
+            <div class="form-group col-xs-12 floating-label-form-group controls">
+                <label>Passport number</label>
+                <input id="passportnumber" class="form-control" type="text" placeholder="Passport number" />
+            </div>
+            <div class="form-group col-xs-12 floating-label-form-group controls">
+                <label>Passport Expiry</label>
+                <input id="passportexpiry" class="form-control" type="date" placeholder="Passport expiry" />
+            </div></div>';
+
         return $passport;
     }
     add_shortcode('cloudcheck_nz_passport', 'cloudcheck_nz_passport_shortcode');
@@ -40,14 +57,21 @@ function cloudcheck_shortcodes_init()
 
 	/** Shortcode that shows fields for verification by NZ driving license:
 	 *  driving license number, driving llicense version
-	 */ 	
+	 */
     function cloudcheck_nz_driving_license_shortcode($atts = [], $content = null)
     {
-        $driving = '<div class="row control-group"><div class="form-group col-xs-12 floating-label-form-group controls"><label>Driver License Number</label><input id="driverlicensenumber" class="form-control" type="text" placeholder="Driver license number" /></div><div class="form-group col-xs-12 floating-label-form-group controls"><label>Driver License Version</label><input id="driverlicenseversion" class="form-control" type="text" placeholder="Driver license version" /></div></div>';
- 
+        $driving = '<div class="row control-group">
+            <div class="form-group col-xs-12 floating-label-form-group controls">
+                <label>Driver License Number</label>
+                <input id="driverlicensenumber" class="form-control" type="text" placeholder="Driver license number" />
+            </div>
+            <div class="form-group col-xs-12 floating-label-form-group controls">
+                <label>Driver License Version</label>
+                <input id="driverlicenseversion" class="form-control" type="text" placeholder="Driver license version" />
+            </div></div>';
+
         return $driving;
     }
     add_shortcode('cloudcheck_nz_driving_license', 'cloudcheck_nz_driving_license_shortcode');
 
 }
-
