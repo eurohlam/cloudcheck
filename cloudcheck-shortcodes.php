5ebe0a1c18142014c8ea809b15ecd9ae4c819df9
eurohlam <eurohlam@gmail.com>
2018-08-08 16:49:34 +1200

    added new shortcodes

--- cloudcheck-shortcodes.php
+++ cloudcheck-shortcodes.php
@@ -29,10 +29,52 @@ function cloudcheck_shortcodes_init()
 	 */
     function cloudcheck_basic_info_shortcode($atts = [], $content = null)
     {
-        // do something to $content
+		$info = '<div class="row control-group">
+                        <div class="form-group col-xs-12 floating-label-form-group controls">
+							<label>Given Name</label>
+                            <input id="name" class="form-control" required type="text" placeholder="Given Name" />
+                        </div>
+                        <div class="form-group col-xs-12 floating-label-form-group controls">
+							<label>Middle Name</label>
+                            <input id="middlename" class="form-control"  type="text" placeholder="Middle Name"/>
+                        </div>
+                        <div class="form-group col-xs-12 floating-label-form-group controls">
+							<label>Family Name</label>
+                            <input id="surname" class="form-control" required type="text" placeholder="Family Name"/>
+                        </div>
+                    </div>
+                    <div class="row control-group">
+                        <div class="form-group col-xs-12 floating-label-form-group controls">
+							<label>Date of birth</label>
+                            <input id="dateofbirth" class="form-control" required type="date" placeholder="Date of birth" />
+                        </div>
+                    </div>
+                    <div class="row control-group">
+                        <div class="form-group col-xs-12 floating-label-form-group controls">
+							<label>Postcode</label>
+                            <input id="postcode" class="form-control" required type="text" placeholder="Postcode" />
+                        </div>
+                        <div class="form-group col-xs-12 floating-label-form-group controls">
+							<label>City</label>
+                            <input id="city" class="form-control" required type="text" placeholder="City"/>
+                        </div>
+                        <div class="form-group col-xs-12 floating-label-form-group controls">
+							<label>Suburb</label>
+                            <input id="suburb" class="form-control" required type="text" placeholder="Suburb"/>
+                        </div>
+                    </div>
+                    <div class="row control-group">
+                        <div class="form-group col-xs-12 floating-label-form-group controls">
+							<label>Street</label>
+                            <input id="street" class="form-control" required type="text" placeholder="Street"/>
+                        </div>
+                        <div class="form-group col-xs-12 floating-label-form-group controls">
+							<label>Street number</label>
+                            <input id="streetnumber" class="form-control" required type="text" placeholder="Street number" />
+                        </div>
+                    </div>';
 
-        // always return
-        return $content;
+        return $info;
     }
     add_shortcode('cloudcheck_basic_info', 'cloudcheck_basic_info_shortcode');
 
@@ -76,4 +118,31 @@ function cloudcheck_shortcodes_init()
     }
     add_shortcode('cloudcheck_nz_driving_license', 'cloudcheck_nz_driving_license_shortcode');
 
+
+	/** Shortcode that shows email fields:
+	 *  Client email, Agent email, Administrator email
+	 */
+    function cloudcheck_emails_shortcode($atts = [], $content = null)
+    {
+        $emails = '<div class="row">
+                       <div class="row control-group">
+                          <div class="form-group col-xs-12 floating-label-form-group controls">
+							  <label>Client email</label>
+                              <input id="clientemail" class="form-control" required type="email" placeholder="Client email"/>
+                          </div>
+                       </div>
+                       <div class="row control-group">
+                          <div class="form-group col-xs-12 floating-label-form-group controls">
+							  <label>Agent email</label>
+                              <input id="agentemail" class="form-control" required type="email" placeholder="Agent email"/>
+                          </div>
+                          <div class="form-group col-xs-12 floating-label-form-group controls">
+							  <label>Administrator email</label>
+                              <input id="adminemail" class="form-control" required type="email" placeholder="Administrator email" />
+                          </div>
+                       </div>
+                    </div>';
+        return $emails;
+    }
+    add_shortcode('cloudcheck_emails', 'cloudcheck_emails_shortcode');
 }
